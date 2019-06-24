b73c1b877f1dfa1b0061fa4f5838bcbce54c8bce
MJ <mtjjoyce@gmail.com>
2019-06-24 02:04:19 +0000

    added functionality to form

--- home.py
+++ home.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
 
 app.config["DEBUG"] = True
 
-@app.route('/')
+email = []
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return(render_template("landing_page.html"))
+    if request.method == "GET":
+        return render_template("landing_page.html", email=email)
+
+    email.append(request.form['contents'])
+    return redirect(url_for(index))
 
