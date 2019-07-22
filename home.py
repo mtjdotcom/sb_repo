031eb1b3a203df39c25ca03c01bdb3a2d150fd23
MJ <mtjjoyce@gmail.com>
2019-07-22 16:14:29 +0000

    added new feedback routes and page

--- home.py
+++ home.py
@@ -24,15 +24,6 @@ class Email(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(4096))
 
-    # __tablename__ = "feedback"
-
-    # id = db.Column(db.Integer, primary_key=True)
-    # q1 = db.Column(db.String(4000))
-    # q2 = db.Column(db.String(4000))
-    # q3 = db.Column(db.String(4000))
-    # q4 = db.Column(db.String(4000))
-    # q5 = db.Column(db.String(4000))
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == "GET":
@@ -43,25 +34,33 @@ def index():
     db.session.commit()
     return redirect(url_for('thanks'))
 
-@app.route('/feedback')#, methods=['GET', 'POST'])
-def feedback():
-    # if request.method == "GET":
-    #     return render_template("feedback.html", email=Email.query.all())
+# @app.route('/home', methods=['GET', 'POST'])
+# def index():
+#     if request.method == "GET":
+#         return render_template("landing_page_home.html", email=Email.query.all())
 
-    # q_1 = Email(q1=request.form['q_1'])
-    # q_2 = Email(q2=request.form['q_2'])
-    # q_3 = Email(q2=request.form['q_3'])
-    # q_4 = Email(q2=request.form['q_4'])
-    # q_5 = Email(q2=request.form['q_5'])
+#     email = Email(content=request.form['contents'])
+#     db.session.add(email)
+#     db.session.commit()
+#     return redirect(url_for('thanks'))
 
-    # db.session.add(q_1)
-    # db.session.add(q_2)
-    # db.session.add(q_3)
-    # db.session.add(q_4)
-    # db.session.add(q_5)
-    # db.session.commit()
-    # return redirect(url_for('thanks'))
-    return render_template('feedback.html')
+# @app.route('/hospital', methods=['GET', 'POST'])
+# def index():
+#     if request.method == "GET":
+#         return render_template("landing_page_hospital.html", email=Email.query.all())
+
+#     email = Email(content=request.form['contents'])
+#     db.session.add(email)
+#     db.session.commit()
+#     return redirect(url_for('thanks'))
+
+@app.route('/feedback_home')
+def feedback_home():
+    return render_template('feedback_home.html')
+
+@app.route('/feedback_business')
+def feedback_business():
+    return render_template('feedback_business.html')
 
 @app.route('/thank_you')
 def thanks():
